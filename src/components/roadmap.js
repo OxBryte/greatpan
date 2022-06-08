d0a20418fb97997f1efec420726515f24927b681
BrightTech <62702022+BrightTechnologies1@users.noreply.github.com>
2022-06-08 04:22:13 +0100

    roadmap update

--- src/components/roadmap.js
+++ src/components/roadmap.js
@@ -11,41 +11,42 @@ function Roadmap() {
                 <div className='col-12'>
                     <h1>Roadmap</h1>
                 </div>
+                {/* <div className='line'></div> */}
                 <div className='row col-12 roadmap-content'>
                     <div className='roadmap-item col-3'>
                         <h3>Phase 1</h3>
                         <ul>
-                            <li>roadmap</li>
-                            <li>roadmap</li>
-                            <li>roadmap</li>
-                            <li>roadmap</li>
+                            <li>Launching of NFT (GreatPan)</li>
+                            <li>GreatPan NFT Collection (2222)</li>
+                            <li>Website Launch</li>
+                            <li>Mint Great Pan</li>
                         </ul>
                     </div>
                     <div className='roadmap-item col-3'>
                         <h3>Phase 2</h3>
                         <ul>
-                            <li>roadmap</li>
-                            <li>roadmap</li>
-                            <li>roadmap</li>
-                            <li>roadmap</li>
+                            <li>Whitepaper Release</li>
+                            <li>Truce Prototype</li>
+                            <li>Marketing</li>
+                            <li>Team Expansion</li>
                         </ul>
                     </div>
                     <div className='roadmap-item col-3'>
                         <h3>Phase 3</h3>
                         <ul>
-                            <li>roadmap</li>
-                            <li>roadmap</li>
-                            <li>roadmap</li>
+                            <li>Truce Landing Page</li>
+                            <li>Development of Truce</li>
+                            <li>Client Patnership</li>
                             <li>roadmap</li>
                         </ul>
                     </div>
                     <div className='roadmap-item col-3'>
                         <h3>Phase 4</h3>
                         <ul>
-                            <li>roadmap</li>
-                            <li>roadmap</li>
-                            <li>roadmap</li>
-                            <li>roadmap</li>
+                            <li>Truce Fair Launch (testnet)</li>
+                            <li>Fixing Bugs (errors)</li>
+                            <li>Truce Fair Launch (Beta-testnet)</li>
+                            <li>Truce Launch</li>
                         </ul>
                     </div>
                 </div>
