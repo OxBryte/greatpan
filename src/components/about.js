7ed208eda4aa8560806a433f57ca36bc41375a91
BrightTechnologies1 <silascyrax@gmail.com>
2022-06-16 16:53:15 +0100

    Updated Responsiveness

--- src/components/about.js
+++ src/components/about.js
@@ -14,16 +14,16 @@ function About() {
             <div className='row'>
                 <h1>About</h1>
             </div>
-            <div className='row col-12 m-0'>
+            <div className='row col-12 m-0 about-item'>
                 <div className='about-image col-sm-12 col-lg-6'>
-                    <img src={about1} alt='' className='col-6' style={{ width: '16rem' }}/>
-                    <img src={about2} alt='' className='col-6' style={{ width: '16rem' }}/>
-                    <img src={about3} alt='' className='col-6' style={{ width: '16rem' }}/>
-                    <img src={about4} alt='' className='col-6' style={{ width: '16rem' }}/>
+                    <img src={about1} alt='' className='col-6'/>
+                    <img src={about2} alt='' className='col-6'/>
+                    <img src={about3} alt='' className='col-6'/>
+                    <img src={about4} alt='' className='col-6'/>
                 </div>
                 <div className='about-text col-sm-12 col-lg-6'> 
                     <h2>WHAT MAKES US DIFFERENT</h2>
-                    <h3>Our aims and concepts are distinct, allowing us to stand out. In various ways, where we deliver and how our platform operates, we are unique in our ambitions and ideas. Our initiative is based on the notion of bringing out the interests and benefits of TRUCE users.<br/><br/>Event organizers from METAVERSE and IRL may use our platform to build their own events, have total control over their events, and use any NFT collection their choice. At the conclusion of each event you produce, you'll get a royalty as a profit.</h3>
+                    <h3>Our aims and concepts are distinct, allowing us to stand out. In various ways, where we deliver and how our platform operates, we are unique in our ambitions and ideas. Our initiative is based on the notion of bringing out the interests and benefits of TRUCE users.<br/>Event organizers from METAVERSE and IRL may use our platform to build their own events, have total control over their events, and use any NFT collection their choice. At the conclusion of each event you produce, you'll get a royalty as a profit.</h3>
                 </div>                    
             </div>
         </div>
