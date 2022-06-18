22a662bddf6b2aa5928258e3b99c1bfa8e2363b8
BrightTechnologies1 <silascyrax@gmail.com>
2022-06-18 01:48:54 +0100

    Updated

--- src/components/banner.js
+++ src/components/banner.js
@@ -8,20 +8,23 @@ function Banner() {
   return (
     <div className="banner">
         <div className='container'>
-            <div className='row navbar'>
+            {/* <div className='row navbar'>
                 <Nav className="justify-content-center" activeKey="/home">
                     <Nav.Item>
                         <Nav.Link href="/home">About</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                        <Nav.Link eventKey="">Roadmap</Nav.Link>
+                        <Nav.Link href="/About">Roadmap</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
                         <Nav.Link eventKey="">Team</Nav.Link>
                     </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="">Faqs</Nav.Link>
+                    </Nav.Item>
                 </Nav>
-            </div>
-            <div className='row col-12'>
+            </div> */}
+            <div className='row banner5 col-12'>
                 <div className='row banner-text col-12'>
                     <h1>GreatPan</h1>
                     <h2>We're building a blockchain-based ticketing platform to make real-world and metaverse events more accessible to those with NFT holdings, which will be available on the NEAR blockchain.</h2>
--- src/components/team.js
+++ src/components/team.js
@@ -18,62 +18,70 @@ function Team() {
                     <h1>Team</h1>
                 </div>
                 <CardGroup className='row col-12'>
-                    <Card style={{ width: '15rem' }} >
-                        <Card.Img variant="top" src={about2} />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Body>
-                            {/* <Card.Link href="#">Card Link</Card.Link> */}
-                            {/* <Card.Link href="#">Another Link</Card.Link> */}
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '15rem' }}>
-                        <Card.Img variant="top" src={about3} />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Body>
-                            {/* <Card.Link href="#">Card Link</Card.Link> */}
-                            {/* <Card.Link href="#">Another Link</Card.Link> */}
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '15rem' }} className='col-md-6'>
-                        <Card.Img variant="top" src={about4} />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Body>
-                            {/* <Card.Link href="#">Card Link</Card.Link> */}
-                            {/* <Card.Link href="#">Another Link</Card.Link> */}
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '15rem' }} className='col-md-6'>
-                        <Card.Img variant="top" src={about1} />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Body>
-                            {/* <Card.Link href="#">Card Link</Card.Link> */}
-                            {/* <Card.Link href="#">Another Link</Card.Link> */}
-                        </Card.Body>
-                    </Card>
+                    <div className='col cardry'>
+                        <Card style={{ width: '15rem' }} >
+                            <Card.Img variant="top" src={about2} />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Body>
+                                {/* <Card.Link href="#">Card Link</Card.Link> */}
+                                {/* <Card.Link href="#">Another Link</Card.Link> */}
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className='col cardry'>
+                        <Card style={{ width: '15rem' }} >
+                            <Card.Img variant="top" src={about2} />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Body>
+                                {/* <Card.Link href="#">Card Link</Card.Link> */}
+                                {/* <Card.Link href="#">Another Link</Card.Link> */}
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className='col cardry'>
+                        <Card style={{ width: '15rem' }} >
+                            <Card.Img variant="top" src={about2} />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Body>
+                                {/* <Card.Link href="#">Card Link</Card.Link> */}
+                                {/* <Card.Link href="#">Another Link</Card.Link> */}
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className='col cardry'>
+                        <Card style={{ width: '15rem' }} >
+                            <Card.Img variant="top" src={about2} />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Body>
+                                {/* <Card.Link href="#">Card Link</Card.Link> */}
+                                {/* <Card.Link href="#">Another Link</Card.Link> */}
+                            </Card.Body>
+                        </Card>
+                    </div>
                 </CardGroup>
             </div>
         </div>
